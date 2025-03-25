365ab181a64c28f8d9e8e93602365561b6800b34
Imants <imants.kokis@gmail.com>
2025-03-25 11:24:07 +0200

    fix: tf output logic

--- output.tf
+++ output.tf
@@ -3,5 +3,5 @@ output "coolify_worker_1_ip" {
 }
 
 output "coolify_worker_2_ip" {
-  value = "${oci_core_instance.coolify_worker_2.public_ip} (use it to add the server in Coolify Dashboard)"
+  value = [for instance in oci_core_instance.coolify_worker_2 : "${oci_core_instance.coolify_worker_2.public_ip} (use it to add the server in Coolify Dashboard)"]
 }
