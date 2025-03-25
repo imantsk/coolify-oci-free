582084aa67aa71201c8a16954c328ca4868e4bfe
Imants <imants.kokis@gmail.com>
2025-03-25 11:31:47 +0200

    fix: update tf outputs

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "coolify_worker_1_ip" {
-  value = "${oci_core_instance.coolify_worker_1.public_ip} (use it to add the server in Coolify Dashboard)"
-}
-
-output "coolify_worker_2_ip" {
-  value = [for instance in oci_core_instance.coolify_worker_2 : "${oci_core_instance.coolify_worker_2.public_ip} (use it to add the server in Coolify Dashboard)"]
+output "coolify_worker_ips" {
+  value = [for instance in oci_core_instance.coolify_worker_2 : "${instance.public_ip} (use it to add the server in Coolify Dashboard)"]
 }
