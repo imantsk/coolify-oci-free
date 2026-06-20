64d89392ae294ef02d83842ccc6ec161f4413484
Imants <imants.kokis@gmail.com>
2026-06-20 19:37:15 +0300

    fix: declare oci and random required providers to resolve init failure

--- providers.tf
+++ providers.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "oci" {}
